abdacca219c8f1c6ff5633583d1fbe81ce8ada68
Nicolas Aimon <nicolas.aimon@withings.com>
2019-05-27 11:14:06 +0200

    simple state machine to arm/disarm/set 1A/set 2A

--- constantload.ino
+++ constantload.ino
@@ -1,31 +1,76 @@
-int CURRENT = 1000; /* mA */
-int RSHUNT = 200; /* mR */
+//int CURRENT = 1000; /* mA */
+//int RSHUNT = 200; /* mR */
+
+// pwm duty cycle (50 <> 1A)
+char DEFAULT_DUTY = 50;
+
+int CUTOFF = 3000;
 
 int dacPin = 9;    // LED connected to digital pin 9
 int currentPin = A0;
 int voltPin = A7;
+
+enum stat {
+  DISARMED,
+  ARMED,
+  DISCHARGING
+};
+
+stat discharging = DISARMED;
 
 void setup() {
   Serial.begin(9600);
   analogReference(INTERNAL);
 
   pinMode(9, OUTPUT);
-  
+
+  // configure Timer1 pwm output, high frequency
   TCCR1A = _BV(COM1A1) | _BV(WGM10);
   TCCR1B = _BV(WGM12) | _BV(CS10);
-
-  OCR1AL = 118;
+  
+  // start with 0% duty cycle
+  OCR1AL = 0;
 }
 
 void loop() {
-  int current, volt;
-
-  current = map(analogRead(currentPin), 0, 1023, 0, 5500);
-  volt = map(analogRead(voltPin), 0, 1023, 0, 4522);
+  if (Serial.available()){
+      switch (Serial.read()) {
+        case '0':
+        case 'd':
+          OCR1AL = 0;
+          discharging = DISARMED;
+          break;
+        case 'a':
+          discharging = ARMED; 
+          break;
+        case '1':
+          if (discharging == ARMED){
+            OCR1AL = 50;
+          }
+          break;
+        case '2':
+          if (discharging == ARMED){
+            OCR1AL = 100;
+          }
+          break;
+      } 
+    }
+    
+    int current, volt;
+    current = map(analogRead(currentPin), 0, 1023, 0, 5500);
+    volt = map(analogRead(voltPin), 0, 1023, 0, 4565);
   
-  Serial.print(current);
-  Serial.print("mA,");
-  Serial.print(volt);
-  Serial.println("mV");
-  delay(1000);
+    Serial.print(millis());
+    Serial.print(',');    
+    Serial.print(current);
+    Serial.print("mA,");
+    Serial.print(volt);
+    Serial.print("mV,");
+    Serial.println(discharging);
+    delay(100);
+
+    if(volt < CUTOFF){
+      OCR1AL = 0;
+      discharging = DISARMED;
+    }
 }
